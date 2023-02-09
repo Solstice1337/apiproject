655b3fa1b52b4ced486007a80b89a9fa5a125d5b
LutherHutchinson <mariabychkova77@gmail.com>
2023-02-09 22:35:07 +0500

    Made getOrganization

--- First_task.py
+++ First_task.py
@@ -11,12 +11,54 @@ class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.getObject)
+        self.pushButton_3.clicked.connect(self.getObject)
         self.pushButton_2.clicked.connect(self.New_Coordinates)
+        self.pushButton.clicked.connect(self.getOrganization)
         self.lon = '37.530887'
         self.lat = '55.703118'
         self.z = '17'
         self.New_Coordinates()
+
+    def getOrganization(self):
+        txt = self.lineEdit_5.text()
+        search_api_server = "https://search-maps.yandex.ru/v1/"
+        api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+        address_ll = ",".join([self.lon, self.lat])
+
+        search_params = {
+            "apikey": api_key,
+            "text": txt,
+            "lang": "ru_RU",
+            "ll": address_ll,
+            "type": "biz"
+        }
+
+        response = requests.get(search_api_server, params=search_params)
+        if not response:
+            self.Picture_place.setText('Ошибка выполнения запроса')
+        json_response = response.json()
+
+        organization = json_response["features"][0]
+        org_name = organization["properties"]["CompanyMetaData"]["name"]
+        org_address = organization["properties"]["CompanyMetaData"]["address"]
+
+        point = organization["geometry"]["coordinates"]
+        self.lon, self.lat = point[0], point[1]
+        org_point = "{0},{1}".format(point[0], point[1])
+        map_params = {
+            "ll": org_point,
+            "z": self.z,
+            "l": self.type,
+            "pt": "{0},ya_ru".format(org_point)
+        }
+
+        map_api_server = "http://static-maps.yandex.ru/1.x/"
+        response = requests.get(map_api_server, params=map_params)
+
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        self.Update_Picture()
 
     def Update_Picture(self):
         pixmap = QPixmap(self.map_file)
@@ -105,7 +147,8 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         map_params = {
             "ll": ",".join([self.lon, self.lat]),
             "z": self.z,
-            "l": self.type
+            "l": self.type,
+            "pt": "{0},ya_ru".format(",".join([self.lon, self.lat]))
         }
 
         map_api_server = "http://static-maps.yandex.ru/1.x/"
