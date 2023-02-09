78091c989d3910c7814e45385cff570d84a2dac4
soui <fine.lolmen@yandex.ru>
2023-02-09 20:19:15 +0500

    added get object

--- First_task.py
+++ First_task.py
@@ -99,6 +99,41 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.getImage()
         event.accept()
 
+    def getObject(self):
+        # тут надо добавить строку поисковую и ее результат записать в эту переменную
+        toponym_to_find = ' '.join()
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": toponym_to_find,
+            "format": "json"}
+
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        if not response:
+            self.Picture_place.setText('Ошибка выполнения запроса')
+
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+        delta = "0.005"
+
+        map_params = {
+            "ll": ",".join([toponym_longitude, toponym_lattitude]),
+            "spn": ",".join([delta, delta]),
+            "l": "map"
+        }
+
+        map_api_server = "http://static-maps.yandex.ru/1.x/"
+        response = requests.get(map_api_server, params=map_params)
+
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        self.Update_Picture()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
